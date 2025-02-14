cc9da65a2f782da21a9fcb2f4d0e1057e6ac3964
DrakBall <oriolav14@gmail.com>
2025-02-15 00:55:08 +0100

    api-key

--- natality+mortality.py
+++ natality+mortality.py
@@ -4,10 +4,10 @@ import hacer_grafico as hg
 
 
 df_nat=ct.csv_treatment("natalitat.csv", "Total")
-pronostico_nat=ia.generate_grafic(df_nat, "Total", "nixak-MNEV6YUxCQmSyIZjfKKmuBMJYQQdacQDh1MPPkpgoQxH2YgNdWR35yl4KpVm3sbt1wnI5FXv7evbax9O")
+pronostico_nat=ia.generate_grafic(df_nat, "Total", "api-key")
 
 df_mort=ct.csv_treatment("mortaldat.csv", "Total")
-pronostico_mort=ia.generate_grafic(df_mort, "Total", "nixak-MNEV6YUxCQmSyIZjfKKmuBMJYQQdacQDh1MPPkpgoQxH2YgNdWR35yl4KpVm3sbt1wnI5FXv7evbax9O")
+pronostico_mort=ia.generate_grafic(df_mort, "Total", "api-key")
 
 ct.csv_save(pronostico_nat, "pronostico_nat.csv")
 ct.csv_save(pronostico_mort, "pronostico_mort.csv")
